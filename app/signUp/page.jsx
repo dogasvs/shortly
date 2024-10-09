6455bfc8c9e67ce79a85b5a86a6e5f01d26e631a
Doğa Savaş <doasavaas@gmail.com>
2024-10-10 00:16:19 +0300

    signup cssi yapıldı

--- app/signUp/page.jsx
+++ app/signUp/page.jsx
@@ -2,10 +2,14 @@ import { signUp } from "@/actions/auth";
 
 export default function SignUpPage() {
   return (
-    <form action={signUp}>
-      <input type="text" name="firstname" placeholder="adınız" /> <br />
-      <input type="text" name="lastname" placeholder="soyadınız" />  <br />
-      <input type="email" name="email" placeholder="email" />  <br />
+    <form className="signUpForm" action={signUp}>
+        <label htmlFor="firstname">Firstname</label>
+      <input type="text" name="firstname" placeholder="Firstname" /> <br />
+      <label htmlFor="lastname">Lastname</label>
+      <input type="text" name="lastname" placeholder="Lastname" />  <br />
+      <label htmlFor="email">Email</label>
+      <input type="email" name="email" placeholder="usermail@gmail.com" />  <br />
+      <label htmlFor="password">password</label>
       <input type="password" name="password" placeholder="********" />
       <button>Sign up</button> 
     </form>
