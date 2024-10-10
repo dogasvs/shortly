1010cd6c7ff962091173f04b61c103d7ad83809c
Doğa Savaş <doasavaas@gmail.com>
2024-10-10 18:26:56 +0300

    log out butonu eklendi ve signout fonksiyonu ile bağlandı

--- components/header/page.jsx
+++ components/header/page.jsx
@@ -1,17 +1,33 @@
 import Link from "next/link";
 import Navbar from "../navbar/page";
+import { createClient } from "@/utils/supabase/server";
+import signOut from "@/actions/auth";
 
-export default function Header() {
+export default async function Header() {
+  const supabase = createClient();
+  const {data: {user}, error} = await supabase.auth.getUser();
+  console.log(user);
+
   return (
     <div className="header">
       <div className="headerSelectedPart">
       <Link href={"/"}> <h1>Shortly</h1> </Link> 
       <Navbar />
       </div>
-      <div className="headerBtns">
-        <Link className="loginBtn" href={"/login"}>Login</Link>
-        <Link className="signBtn" href={"/signUp"}>Sign in</Link>
-      </div> 
+
+        { user ? (
+           <div className="userBtns">
+            <p>{user.user_metadata.firstName} {user.user_metadata.lastname}</p>
+            <form action={signOut}>
+              <button className="logOut">Log out</button>
+            </form>
+         </div>
+        ) : (
+          <div className="headerBtns">
+            <Link className="loginBtn" href={"/login"}>Login</Link>
+            <Link className="signBtn" href={"/signUp"}>Sign in</Link>
+          </div> 
+        )}
     </div>
   )
 }
--- components/result/page.jsx
+++ components/result/page.jsx
@@ -4,17 +4,19 @@ import React from 'react';
 export default async function ShortUrlDisplay() {
   const supabase = createClient();
   const { data: userdata, error: usererror } = await supabase.auth.getUser();
+
   const { data, error } = await supabase
     .from('urls')
     .select('*')
-    .eq("user_id", userdata.user?.id); //karsilastirma operatörü
+    .eq("user_id", userdata.user?.id); 
+
   console.log(data);
   console.log(userdata);
 
   return (
     <div className="shortUrls">
      {data?.map((x, i) => (
-        <div key={i}>{x.long_url}</div>
+        <div key={i}>{x.short_url}</div>
      ))  
     }
     </div>
