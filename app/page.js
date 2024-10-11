96a246c9aa9f26d9d325cb87e4b6b6eb990a0a1a
Doğa Savaş <doasavaas@gmail.com>
2024-10-11 12:48:34 +0300

    layouttakiler page.jse taşındı forma prop gönderildi

--- app/page.js
+++ app/page.js
@@ -1,18 +1,21 @@
 import Footer from "@/components/footer/page.";
 import Form from "@/components/form/page";
 import LinkPromotion from "@/components/linkPromotion/page";
-import ShortUrlDisplay from "@/components/result/page";
 import Statistics from "@/components/statistics/page";
 import Subheader from "@/components/subheader/page";
+import { createClient } from "@/utils/supabase/server";
+import ShortUrlDisplay from "./result";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient(); 
+  const {data: {user}, error} = await supabase.auth.getUser();
   return (
     <>
       <div className="container">
         <div className="content">
           <Subheader />
         </div>
-        <Form />
+        <Form user={user} />
         <ShortUrlDisplay />
         <Statistics />
       </div>
