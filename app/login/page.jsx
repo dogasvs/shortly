1d6b29fdb8c23b3b999cdf92741a40dfa5e41a55
Doğa Savaş <doasavaas@gmail.com>
2024-10-11 12:47:01 +0300

    loginpagete formaction kaldırıldı yerine action kullanıldı

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,12 +4,12 @@ export default function LoginPage() {
   return (
     <div className="loginContainer">
 
-    <form className="loginForm">
+    <form className="loginForm" action={ login }>
       <label htmlFor="email">Email</label>
       <input id="email" name="email" type="email" required placeholder="usermail@gmail.com" />
       <label htmlFor="password">Password</label>
       <input id="password" name="password" type="password" required placeholder="*****" />
-      <button formAction={ login }>Log in</button>
+      <button>Log in</button>
     </form>
     </div>
   )
